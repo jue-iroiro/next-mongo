121d642aefa8d746d4644ac3b021ba7b1140481b
Jue Jue <juejue@Jues-MacBook-Air.local>
2024-09-15 15:24:30 +0700

    category_delete

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,7 +1,7 @@
 import Category from "@/models/Category";
 
 export async function GET() {
-  const categories = await Category.find().sort({name:1})
+  const categories = await Category.find().sort({order:-1})
   return Response.json( categories)
 }
 
@@ -11,3 +11,10 @@ export async function POST(request) {
   await category.save()
   return Response.json(category)
 }
+
+export async function PUT(request) {
+  const body = await request.json()
+  const category = await Category.findByIdAndUpdate(body._id, body)
+  return Response.json(category)
+}
+
